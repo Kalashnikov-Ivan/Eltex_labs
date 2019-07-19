57dcc219b62c88d2660a52772e177a9381ccc0e8
Kalashnikov Ivan <steindvart.ivan@gmail.com>
2019-07-19 11:34:56 +0700

    Start

--- lab_4/1/main.c
+++ lab_4/1/main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <stddef.h> //For size_t, ptrdiff_t and so on...
 
 #define SIZE_FILENAME 128UL
@@ -44,7 +45,7 @@ int main(int argc, char *argv[])
 
 	/*printf("%s\n", new_filename);
 
-	char out;
+	int out;
 	while ((out = fgetc(fp_in)) != EOF)
 	{
 		printf("%c\n", out);
@@ -55,45 +56,27 @@ int main(int argc, char *argv[])
 		printf("%ld\n", fp_in->_IO_read_ptr);
 	}
 	*/
-	unsigned long q_symbols_file = 0;
 
-	char out;
-	unsigned int q = 0;
-	while (((out = fgetc(fp_in)) != EOF) && q != q_str)
+	FILE *fp_out = fopen(new_filename, "w");
+
+	int out, bool_with_num = 0;
+
+	for (size_t i = 0, j = 0; out != EOF;)
 	{
-		if (out == '\n')
-			q++;
-		q_symbols_file++;
-	}
+		while (out = getc(fp_in))
+		{
+			if (isdigit(out))
+				bool_with_num = 1;
 
-	char *buff_file = (char*)malloc(sizeof(char) * q_symbols_file);
-
-	for (i = 0, q = 0; ((buff_file[i] = fgetc(fp_in)) != EOF) && q != q_str; i++)
-	{
-		if (buff_file[i] == '\n')
-			q++;
-	}
-
-	buff_file[i] = '\0';
-
-	printf("%s", buff_file);
-
-
-	/*FILE fp_out = fopen(new_filename, "w");
-
-	char out, bool_with_num = 0;
-
-	for (size_t i = 0, j = 0; (out = getc(fp_in)) != EOF; i++)
-	{
-
+		}
 	}
 
 	if(fclose(fp_in))
 	{ 
 		printf("Error of close file.\n");
 		exit(1);
-	}*/
-
+	}
+	
 
 	printf("\n----Programm is finished----\n");
 	return 0;
