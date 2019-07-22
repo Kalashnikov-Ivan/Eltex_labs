d86234d0acd9547ab1341c452c9c480de91729e1
Kalashnikov Ivan <steindvart.ivan@gmail.com>
2019-07-22 16:46:17 +0700

    dynamic started

---
+++ lab_5/dynamic_1/main.c
@@ -0,0 +1,29 @@
+/*
+
+		Author: Kalashnikov Ivan
+		Task: Lab_5, Var 6.3 (dynamic)
+
+*/
+
+
+#include <stdio.h>
+
+#include "/home/st/Work/Eltex/C/lab_5/myLib/source/str_op.h"
+
+#define STR_SIZE 128UL
+
+int main(void) 
+{
+	char str[STR_SIZE] = "hello, ";
+	str_conc(str, STR_SIZE, "dynamic libs");
+
+	printf("%s\n", str);
+
+
+	char str2[STR_SIZE];
+	str_gap(str2, STR_SIZE, str, 8);
+
+	printf("%s, bye!\n", str2);
+
+	return 0;
+}
